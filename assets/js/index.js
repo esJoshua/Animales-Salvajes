f2fa7e18c81511108eb3d73303e97fe339197af5
Joshua Espinoza <josh6669@gmail.com>
2022-03-07 23:16:46 -0300

    improving the code & logic of functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,9 +12,6 @@ const previewId = document.getElementById("preview");
 const btnRegistrarId = document.getElementById("btnRegistrar");
 const playerId = document.getElementById("player");
 const exampleModalId = document.getElementById("exampleModal");
-//const dataInvest = [];
-let animal = [];
-let arrayAnimales = "";
 
 /* FUNCIÓN PARA LIMPIAR EL FORMULARIO */
 const resetForm = () => {
@@ -23,11 +20,6 @@ const resetForm = () => {
   comentariosId.value = "";
   previewId.removeAttribute("style");
 };
-/* FUNCIÓN PARA ENCONTRAR COINCIDENCIA CON LA DATA DEL JSON */
-const funcionFind = () => {
-  const animalFind = arrayAnimales.find(({ name }) => name === animalId.value);
-  return animalFind;
-};
 
 /* FUNCIÓN ASYNC / AWAIT PARA EXTRAER DATA DEL ARCHIVO .JSON */
 const getAnimalesJson = (async () => {
@@ -35,77 +27,86 @@ const getAnimalesJson = (async () => {
     const URL = "assets/animales.json";
     const request = await fetch(URL);
     const data = await request.json();
-    arrayAnimales = data.animales;
-    //console.log(arrayAnimales);
+    return data;
+    //console.log(data);
   } catch (error) {
     console.error(error);
   }
 })();
 
-/* IMAGEN PREVIEW */
-animalId.addEventListener("change", () => {
-  //console.log(funcionFind());
-  previewId.style.backgroundImage =
-    "url(assets/imgs/" + funcionFind().imagen + ")";
-});
+const getdata = (async () => {
+  const data = await getAnimalesJson;
+  const arrayAnimales = data.animales;
 
-/* REGISTRAR INFO AL DAR CLICK AL BOTON */
-btnRegistrarId.addEventListener("click", () => {
-  //console.log(animalId.value)
-  //console.log(!comentariosId.value.trim())
+  /* FUNCIÓN PARA ENCONTRAR COINCIDENCIA CON LA DATA DEL JSON */
+  const funcionFind = () => {
+    const animalFind = arrayAnimales.find(
+      ({ name }) => name === animalId.value
+    );
+    return animalFind;
+  };
 
-  /* VALIDACIONES DEL FORMULARIO */
-  if (
-    animalId.value == "Seleccione un animal" ||
-    edadId.value == "Seleccione un rango de años" ||
-    !comentariosId.value.trim()
-  )
-    return alert("Debe completar todos los campos del formulario");
-  else {
-    const datosInstanciacion = [
-      funcionFind().name,
-      edadId.value,
-      funcionFind().imagen,
-      comentariosId.value,
-      funcionFind().sonido,
-    ];
-    /* INSTANCIACIONES */
-    switch (animalId.value) {
-      case "Leon":
-        animal = new Leon(...datosInstanciacion);
-        break;
-      case "Lobo":
-        animal = new Lobo(...datosInstanciacion);
-        break;
-      case "Oso":
-        animal = new Oso(...datosInstanciacion);
-        break;
-      case "Serpiente":
-        animal = new Serpiente(...datosInstanciacion);
-        break;
-      case "Aguila":
-        animal = new Aguila(...datosInstanciacion);
-        break;
-      default:
-        break;
+  /* IMAGEN PREVIEW */
+  animalId.addEventListener("change", () => {
+    //console.log(funcionFind());
+    previewId.style.backgroundImage =
+      "url(assets/imgs/" + funcionFind().imagen + ")";
+  });
+
+  /* REGISTRAR INFO AL DAR CLICK AL BOTON */
+  btnRegistrarId.addEventListener("click", () => {
+    //console.log(animalId.value)
+    //console.log(!comentariosId.value.trim())
+
+    /* VALIDACIONES DEL FORMULARIO */
+    if (
+      animalId.value == "Seleccione un animal" ||
+      edadId.value == "Seleccione un rango de años" ||
+      !comentariosId.value.trim()
+    )
+      return alert("Debe completar todos los campos del formulario");
+    else {
+      let animal = "";
+      const datosInstanciacion = [
+        funcionFind().name,
+        edadId.value,
+        funcionFind().imagen,
+        comentariosId.value,
+        funcionFind().sonido,
+      ];
+      /* INSTANCIACIONES */
+      switch (animalId.value) {
+        case "Leon":
+          animal = new Leon(...datosInstanciacion);
+          break;
+        case "Lobo":
+          animal = new Lobo(...datosInstanciacion);
+          break;
+        case "Oso":
+          animal = new Oso(...datosInstanciacion);
+          break;
+        case "Serpiente":
+          animal = new Serpiente(...datosInstanciacion);
+          break;
+        case "Aguila":
+          animal = new Aguila(...datosInstanciacion);
+          break;
+        default:
+          break;
+      }
+      //console.log(animal);
+      crearCards(animal);
+      resetForm();
     }
-    // dataInvest.push(animal);
-    //console.log(dataInvest);
-    //console.log(animal);
-    cardsBoard.cards(animal);
-    resetForm();
-  }
-});
+  });
 
-/* INSERTANDO IMAGEN EN EL BOARD DE ANIMALES EN INVEST. */
-let cardsBoard = (() => {
-  let crearCards = (animal) => {
-    console.log(animal);
-
+  /* INSERTANDO IMAGEN EN EL BOARD DE ANIMALES EN INVEST. */
+  let crearCards = (a) => {
+    console.log(a);
     animalesId.innerHTML += `        
         <div class="card col-12 col-md-3 p-0 m-1">
-            <img class="card-img-top" src="assets/imgs/${funcionFind().imagen}" 
-            height="250px" width="180px" alt="animalId.value">
+            <img class="card-img-top" src="assets/imgs/${a.img}" 
+            height="250px" width="180px" alt="${a.name}">
             <div class="card-footer p-0 w-100">
                 <button id="btnSonido" class="btn btn-secondary btn-block w-100">
                     <img src="assets/imgs/audio.svg" style="width: 30px" />
@@ -114,5 +115,4 @@ let cardsBoard = (() => {
         </div>        
         `;
   };
-  return { cards: (animal) => crearCards(animal) };
 })();
